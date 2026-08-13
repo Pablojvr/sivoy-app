1c4bd779ded655e0afffc97aec00e54a13cb6f90
Pablojvr <nsxavi040jp.jp@gmail.com>
2026-08-13 16:17:55 -0600

    Cleanup repo and fix address UI layout

--- backend/query_soya.js
+++
@@ -1,6 +0,0 @@
-const sqlite3 = require('sqlite3'); 
-const db = new sqlite3.Database('../data/sivoyapp.sqlite'); 
-db.all('SELECT * FROM agencias WHERE municipio="Soyapango"', [], (err, rows) => { 
-  console.log(JSON.stringify(rows, null, 2)); 
-  db.close(); 
-});
--- backend/scripts/dump_schema.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('a:/SiVoyApp/data/sivoyapp.sqlite');
-db.all("SELECT name, sql FROM sqlite_master WHERE type='table'", (err, rows) => {
-    if (err) console.error(err);
-    else rows.forEach(r => console.log(r.sql));
-    db.close();
-});
